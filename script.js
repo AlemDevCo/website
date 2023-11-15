977b73f616a6e356b23bb4d04577a3c970270269
AlemDevCo <102746202+AlemDevCo@users.noreply.github.com>
2023-11-15 15:38:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function uploadFile() {
 
     if (file) {
         var reader = new FileReader();
-        
+
         reader.onload = function(e) {
             var content = e.target.result;
 
-            // Parse RBXM or XML content
-            var parsedData = parseContent(content);
+            try {
+                // Parse RBXM or XML content
+                var parsedData = parseContent(content);
 
-            // Render GUI on the webpage
-            renderGUI(parsedData, guiContainer);
+                // Render GUI on the webpage
+                renderGUI(parsedData, guiContainer);
+            } catch (error) {
+                alert('Error parsing the file: ' + error.message);
+            }
         };
 
         reader.readAsText(file);
@@ -25,10 +29,53 @@ function uploadFile() {
 
 function parseContent(content) {
     // Implement RBXM parsing logic using DOMParser
-    // Return the parsed data
+    // For simplicity, let's assume RBXM file is XML-like
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(content, 'text/xml');
+
+    // Extract relevant data from the parsed XML
+    // For example, assuming the top-level element is ScreenGui
+    var screenGuiData = parseScreenGui(xmlDoc);
+
+    return screenGuiData;
+}
+
+function parseScreenGui(xmlDoc) {
+    // Extract ScreenGui data
+    // For simplicity, let's assume ScreenGui has child elements like Frame, TextLabel, etc.
+    var screenGuiData = {
+        type: 'ScreenGui',
+        children: []
+    };
+
+    var elements = xmlDoc.getElementsByTagName('ScreenGui')[0].children;
+
+    for (var i = 0; i < elements.length; i++) {
+        var elementType = elements[i].tagName;
+        var elementData = {
+            type: elementType,
+            // Extract other relevant data based on your file structure
+        };
+
+        screenGuiData.children.push(elementData);
+    }
+
+    return screenGuiData;
 }
 
 function renderGUI(data, container) {
     // Implement dynamic rendering logic based on the parsed data
-    // Update the content of the container with the rendered GUI
+    container.innerHTML = ''; // Clear previous content
+
+    var guiElement = document.createElement(data.type);
+
+    for (var i = 0; i < data.children.length; i++) {
+        var childData = data.children[i];
+        var childElement = document.createElement(childData.type);
+        // Set attributes and styles based on childData
+
+        guiElement.appendChild(childElement);
+    }
+
+    container.appendChild(guiElement);
 }
